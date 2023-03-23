c7a3b79aea7ef5523c962b01673f07a04eb41542
ksekret <MY_NAME@example.com>
2023-03-23 10:23:15 +0300

    renameing service

--- deploy.cmd
+++ deploy.cmd
@@ -3,12 +3,12 @@ FOR /F %%I IN ('git pull') DO @SET "MY_VAR=%%I"
 echo %MY_VAR%
 if /i "%MY_VAR%"=="Already" GOTO end
 
-nssm stop ipgusers
-REM nssm remove ipgusers confirm
+nssm stop ipg
+REM nssm remove ipg confirm
 git pull
 call npm run build
-REM nssm install ipgusers "c:\Program Files\nodejs\node.exe" "C:\projects\ipg_scripter\dist\main"
-nssm start ipgusers
+REM nssm install ipg "c:\Program Files\nodejs\node.exe" "C:\projects\ipg_scripter\dist\main"
+nssm start ipg
 
 :end
 echo END
