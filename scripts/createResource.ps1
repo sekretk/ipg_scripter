d256fb69d8a1c774b1ac1dc668da8bfef6c367f3
ksekret <MY_NAME@example.com>
2023-04-12 18:25:36 +0300

    added script

---
+++ scripts/createResource.ps1
@@ -0,0 +1,11 @@
+﻿$folder = $args[0];
+$group = $args[1];
+New-ADGroup -Name $group -GroupScope Universal
+New-Item -Path $folder -Type Directory
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($group,"Modify", "ContainerInherit,ObjectInherit", "None", "Allow")
+$acl = (Get-ACL -Path $folder)
+$acl.SetAccessRuleProtection($True, $True)
+$domainUsers = New-Object System.Security.Principal.Ntaccount("Domain Users")
+$acl.PurgeAccessRules($domainUsers)
+$acl.SetAccessRule($AccessRule)
+Set-Acl -Path $folder -AclObject $acl
