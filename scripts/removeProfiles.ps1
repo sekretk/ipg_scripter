136e4af0f59cd7133d3419ef5f1c9cd2425f6f83
ksekret <MY_NAME@example.com>
2023-04-13 17:31:04 +0300

    delete deattached profiles

---
+++ scripts/removeProfiles.ps1
@@ -0,0 +1,19 @@
+﻿$files = Get-ChildItem "D:\"
+
+for ($i=0; $i -lt $files.Count; $i++) {
+$length = $files[$i].Name.length
+    $sid = $files[$i].Name.Substring(5, $length-10)
+    try {
+    $user = Get-ADUser -Identity $sid
+    $UserExists = $true
+}
+catch [Microsoft.ActiveDirectory.Management.ADIdentityResolutionException] {
+    Write-Host "User does not exist." + $sid
+    $UserExists = $false
+$vhd = "D:\UVHD-" + $sid.Trim() + ".vhdx"
+
+if (Test-Path -Path $vhd -PathType Leaf) {
+    Remove-Item -Path $vhd  -Force 
+}
+}
+}
