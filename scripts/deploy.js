9d2a956354c921c501c020a9adf899b225f99479
Konstantin Sekret <ksekret@devexperts.com>
2023-05-02 15:11:53 +0300

    fix

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const { execSync } = require("child_process");
+const fs = require('fs');
+
+const curVersion = fs.readFileSync('.version', 'utf8');
+execSync('git pull');
+
+const pulledVersion = fs.readFileSync('.version', 'utf8');
+
+if (pulledVersion === curVersion) {
+    return;
+}
+
+execSync('cd ../client');
+execSync('npm i');
+execSync('npm run build');
+execSync('cd ../server');
+execSync('npm i');
+execSync('npm run build');
+execSync('cd ..');
+execSync('cp -rf server/public server/dist/public');
+execSync('cp -rf server/views server/dist/views');
+execSync('cp -rf client/build/* server/dist/public/');
+execSync('cp .env server/dist/.env');
+
+const changeLog = execSync(`git log --oneline --pretty=format:%s ${curVersion}..HEAD`, { encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 })
+const changeSet = changeLog.split('\n').filter(msg => msg.startsWith('(d)'));
+
+//send to slack
--- scripts/release.js
+++ scripts/release.js
@@ -1,7 +1,28 @@
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
 const fs = require('fs');
 
 const curVersion = fs.readFileSync('.version', 'utf8');
+execSync('git stash');
 
-exec('git pull');
+execSync('git checkout release');
 
+execSync('git pull');
+
+const nextVersion = Number(curVersion)++ ;
+
+fs.writeFile('.version', nextVersion);
+
+execSync('git add .version');
+execSync(`git commit -m"Release ${nextVersion}"`);
+execSync(`git push`);
+
+execSync(`git tag ${nextVersion}`);
+execSync(`git push orig ${nextVersion}`);
+
+const changeLog = execSync(`git log --oneline --pretty=format:%s ${curVersion}..HEAD`, { encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 })
+
+const changeSet = changeLog.split('\n').filter(msg => msg.startsWith('(d)'));
+
+console.log(`Released ${nextVersion} ${changeSet.join('\n')}`)
+
+execSync('git stash pop');
