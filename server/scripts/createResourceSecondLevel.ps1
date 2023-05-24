a556bbcdc770b31c909a28af2184b84e0adcb9b1
ksekret <MY_NAME@example.com>
2023-05-24 22:34:33 +0300

    scripts

---
+++ server/scripts/createResourceSecondLevel.ps1
@@ -0,0 +1,23 @@
+$folder = $args[0];
+$group = $args[1];
+$parentGroup = $args[2];
+
+New-ADGroup -Name $group -GroupScope Universal
+
+Add-ADGroupMember -Identity "${parentGroup}" -Members ${group}
+
+New-Item -Path $folder -Type Directory
+
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($group,"Modify", "ContainerInherit,ObjectInherit", "None", "Allow")
+$acl = (Get-ACL -Path $folder)
+$acl.SetAccessRule($AccessRule)
+Set-Acl -Path $folder -AclObject $acl
+
+$acl = (Get-ACL -Path $folder)
+$acl.SetAccessRuleProtection($True, $True)
+Set-Acl -Path $folder -AclObject $acl
+
+$acl = (Get-ACL -Path $folder)
+$domainUsers = New-Object System.Security.Principal.Ntaccount("Domain Users")
+$acl.PurgeAccessRules($domainUsers)
+Set-Acl -Path $folder -AclObject $acl
--- server/scripts/createResourceWithRoot.ps1
+++ server/scripts/createResourceWithRoot.ps1
@@ -1,4 +1,11 @@
- 
+$folder = $args[0];
+$group = $args[1];
+$parentFolder = $args[2];
+$parentGroup = $args[3];
+
+New-ADGroup -Name $group -GroupScope Universal
+New-ADGroup -Name $parentGroup -GroupScope Universal
+
 Add-ADGroupMember -Identity "${parentGroup}" -Members ${group}
 
 New-Item -Path $parentFolder -Type Directory
@@ -6,10 +13,6 @@ New-Item -Path $parentFolder -Type Directory
 New-Item -Path $folder -Type Directory
 
 $AccessRuleRE = New-Object System.Security.AccessControl.FileSystemAccessRule($parentGroup, "ReadAndExecute", "ContainerInherit,ObjectInherit", "None", "Allow")
-$acl = (Get-ACL -Path $parentFolder)
-$acl.SetAccessRule($AccessRuleRE)
-
-$AccessRuleRE = New-Object System.Security.AccessControl.FileSystemAccessRule($parentGroup, "ListDirectory", "ContainerInherit,ObjectInherit", "None", "Allow")
 $acl = (Get-ACL -Path $parentFolder)
 $acl.SetAccessRule($AccessRuleRE)
 
@@ -26,10 +29,11 @@ $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($gro
 $acl = (Get-ACL -Path $folder)
 $acl.SetAccessRule($AccessRule)
 Set-Acl -Path $folder -AclObject $acl
+
 $acl = (Get-ACL -Path $folder)
 $acl.SetAccessRuleProtection($True, $True)
 Set-Acl -Path $folder -AclObject $acl
 $acl = (Get-ACL -Path $folder)
 $domainUsers = New-Object System.Security.Principal.Ntaccount("Domain Users")
 $acl.PurgeAccessRules($domainUsers)
-Set-Acl -Path $folder -AclObject $acl 
+Set-Acl -Path $folder -AclObject $acl
