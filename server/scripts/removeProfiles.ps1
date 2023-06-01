96cc23701c2698d358fb5fc29ea60379e529ec78
Konstantin Sekret <ksekret@devexperts.com>
2023-06-01 11:39:01 +0300

    fix remove profiles

--- server/scripts/removeProfiles.ps1
+++ server/scripts/removeProfiles.ps1
@@ -1,19 +1,19 @@
-﻿$files = Get-ChildItem "D:\"
+﻿$files = Get-ChildItem "D:\" | Where-Object { $_.Name -like "*template*" }
 
 for ($i=0; $i -lt $files.Count; $i++) {
-$length = $files[$i].Name.length
+    $length = $files[$i].Name.length
     $sid = $files[$i].Name.Substring(5, $length-10)
     try {
     $user = Get-ADUser -Identity $sid
     $UserExists = $true
-}
-catch [Microsoft.ActiveDirectory.Management.ADIdentityResolutionException] {
-    Write-Host "User does not exist." + $sid
-    $UserExists = $false
-$vhd = "D:\UVHD-" + $sid.Trim() + ".vhdx"
+    }
+    catch [Microsoft.ActiveDirectory.Management.ADIdentityResolutionException] {
+        Write-Host "User does not exist." + $sid
+        $UserExists = $false
+        $vhd = "D:\UVHD-" + $sid.Trim() + ".vhdx"
 
-if (Test-Path -Path $vhd -PathType Leaf) {
-    Remove-Item -Path $vhd  -Force 
-}
-}
+        if (Test-Path -Path $vhd -PathType Leaf) {
+            Remove-Item -Path $vhd  -Force 
+        }
+    }
 }
